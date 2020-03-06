217df11bef802be6ea03f7c943756a8b753f0bc6
Johannes Waigel <johannes@waigel.me>
2020-03-06 21:15:34 +0100

    Update module.php

--- RCT_POWER_INVERTER/module.php
+++ RCT_POWER_INVERTER/module.php
@@ -482,7 +482,7 @@
 	    $hexCommand = $hexCommand.chr(hexdec(substr( $command, $x*2, 2 )));
 				 
 	  $expectedLength = 9 + $length; // 
-		$this->sendDebug( "TCP SEND kl ", json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", 
+	$this->sendDebug( "TCP SEND kl ", json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", 
 		"Buffer" => utf8_encode($hexCommand) )));
 	  // clear expected Response and send Data to Parent...
 	  $this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", 
